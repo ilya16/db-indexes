28750a764c47cab876afcb8f1752649b1a288266
ilya16 <ilyaborovik97@gmail.com>
2018-02-13 13:31:14 +0300

    list generators and test improvements

--- index_test.py
+++ index_test.py
@@ -1,6 +1,6 @@
 import timeit
 
-from tables.list_generators import get_long_list_of_items, get_item_of_interest
+from tables.list_generators import get_list_of_items, get_list_with_string_keys, get_item_of_interest
 from indexes.naive_index import NaiveIndex
 from indexes.bitmap_index import BitmapIndex
 from indexes.btree import BTree
@@ -9,62 +9,72 @@ from indexes.hash_index import HashIndex
 # generate a list L of items with len(L) = 20 000
 # each item has a key() and data() methods
 list_size = 1000
-a_long_list = get_long_list_of_items(list_size)
+# a_long_list = get_list_of_items(list_size)
+a_long_list = get_list_with_string_keys(list_size)
 
 # a random item from the list (L.index(item) > len(L) / 2)
 item_of_interest = get_item_of_interest(a_long_list)
 print("Random item: ({0}, {1})".format(item_of_interest.key(), item_of_interest.value()))
 
-print(" =============== Naive Search =============== ")
+print("\n =============== Naive Search =============== ")
 
 naive_index = NaiveIndex(a_long_list)
 
 # measure naive time
 start = timeit.default_timer()
 found_item = naive_index.look_up(item_of_interest)
-print("Elapsed time for Loop: {}".format(timeit.default_timer() - start))
+t_no_idx = timeit.default_timer() - start
 # end measure naive time
 
+print("Elapsed time for Loop: {}".format(t_no_idx))
 print("Got item: ({0}, {1})".format(found_item.key(), found_item.value()))
 
 
-print(" =============== Bitmap Index =============== ")
+print("\n =============== Bitmap Index =============== ")
 
 bitmap_index = BitmapIndex(a_long_list)
 
 # measure index lookup term
 start = timeit.default_timer()
 found_items = bitmap_index.look_up(item_of_interest)
-print("Elapsed time for Bitmap index: {}".format(timeit.default_timer() - start))
+t_bitmap_idx = timeit.default_timer() - start
 # end measure index lookup term
+
+print("Elapsed time for Bitmap index: {}".format(t_bitmap_idx))
+print("Faster than Naive Search in {:.2f} times".format(t_no_idx/t_bitmap_idx))
 
 for item in found_items:
     print("Got item: ({0}, {1})".format(item.key(), item.value()))
 
 
-print(" =============== BTree Index  =============== ")
+print("\n =============== BTree Index  =============== ")
 
 btree_index = BTree(a_long_list)
 
 # measure index lookup term
 start = timeit.default_timer()
 found_item = btree_index.look_up(item_of_interest)
-print("Elapsed time for Btree index: {}".format(timeit.default_timer() - start))
+t_btree_idx = timeit.default_timer() - start
 # end measure index lookup term
+
+print("Elapsed time for Btree index: {}".format(t_btree_idx))
+print("Faster than Naive Search in {:.2f} times".format(t_no_idx/t_btree_idx))
 
 print("Got item: ({0}, {1})".format(found_item.key, found_item.value))
 
 
-print(" =============== Hash Index =============== ")
+print("\n =============== Hash Index =============== ")
 
 hash_index = HashIndex(a_long_list)
-# hash_index.print_index()
 
 # measure index lookup term
 start = timeit.default_timer()
 found_items = hash_index.look_up(item_of_interest)
-print("Elapsed time for Hash index: {}".format(timeit.default_timer() - start))
+t_hash_idx = timeit.default_timer() - start
 # end measure index lookup term
+
+print("Elapsed time for Hash index: {}".format(t_hash_idx))
+print("Faster than Naive Search in {:.2f} times".format(t_no_idx/t_hash_idx))
 
 for item in found_items:
     print("Got item: ({0}, {1})".format(item.key(), item.value()))
--- tables/list_generators.py
+++ tables/list_generators.py
@@ -1,32 +1,52 @@
 from random import randint
-from .item import Item
+from uuid import UUID, uuid4
+from tables.item import Item
 
 
-def get_long_list_of_items(list_size):
-    """ Generates a list with defined data. """
-    item_list = []
-    for i in range(list_size):
-        if i % 3 == 0:
-            item_list.append(Item(i, "Programming"))
-        elif i % 3 == 1:
-            item_list.append(Item(i, "Sports"))
-        else:
-            item_list.append(Item(i, "Music"))
-    return item_list
+def generate_item_value(key):
+    """
+    Generates an Item value by the key.
+    :param key:     key of the item
+    :return:        created item
+    """
+    if key % 3 == 0:
+        return "Programming"
+    elif key % 3 == 1:
+        return "Sports"
+    else:
+        return "Music"
+
+
+def generate_item(key):
+    """
+    Generates an Item object by the key.
+    :param key:     key of the item
+    :return:        created item
+    """
+    return Item(key, generate_item_value(key))
+
+
+def get_list_of_items(list_size):
+    """
+    Generates a list of items with defined data.
+    :param list_size:   size of the list
+    :return:            generated list of items
+    """
+    return [generate_item(i) for i in range(list_size)]
 
 
 def get_list_of_random_items(list_size, max_number=10000):
-    """ Generates a list with random keys. """
-    item_list = []
-    for i in range(list_size):
-        key = randint(0, max_number)
-        if key % 3 == 0:
-            item_list.append(Item(key, "Programming"))
-        elif key % 3 == 1:
-            item_list.append(Item(key, "Sports"))
-        else:
-            item_list.append(Item(key, "Music"))
-    return item_list
+    """
+    Generates a list of items with random keys.
+    :param list_size:   size of the list
+    :param max_number:  the upper bound range of integer keys
+    :return:            generated list of items
+    """
+    return [generate_item(randint(0, max_number)) for i in range(list_size)]
+
+
+def get_list_with_string_keys(list_size):
+    return [Item(uuid4(), generate_item_value(i)) for i in range(list_size)]
 
 
 def get_item_of_interest(list_of_items, item_index=None):
@@ -38,7 +58,7 @@ def get_item_of_interest(list_of_items, item_index=None):
     :return:                item of interest
     """
 
-    if not item_index:
+    if item_index is None:
         return list_of_items[randint(int(len(list_of_items) / 2), len(list_of_items))]
     else:
         return list_of_items[item_index]
